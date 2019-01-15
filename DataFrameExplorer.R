575c33f3f3315b41d3205c4a0208e5efa37a9adf
Nikolaus Thiel <Nikolaus.Thiel@homegate.ch>
2019-01-15 16:48:37 +0100

    Number of filters can vary

--- DataFrameExplorer.R
+++ DataFrameExplorer.R
@@ -42,7 +42,7 @@ getData <- function(dimensionen,desiredCols) {
     ### Wenn nur 1 kpi ausgewaehlt, dann heisst diese Spalte x, was aber nicht sein sollte
     if (1==length(foundKpis)) {colnames(resultat) <- gsub(x=colnames(resultat),pattern="x",replacement=foundKpis[1])}
   }
-  loggResultatDf("getData",df=resultat)
+  loggResultatDf(FUN="getData",df=resultat)
   return(resultat)
 }
 
@@ -62,16 +62,17 @@ tempusSelector <- function(INPUTID,NOMEN,AB,USQUEAD,INITIUM,tf){
               ,step=1
               ,width='100%')}
 
-getSelector <- function(INPUTID,NOMEN,delectendum) {
+getSelector <- function(INPUTID,NOMEN,delectendum,multi=FALSE) {
   return(selectInput(inputId=INPUTID
                      ,label=NOMEN
                      ,selected=delectendum[1]
                      ,selectize=TRUE
-                     ,multiple=FALSE
+                     ,multiple=multi
                      ,choices=delectendum))
 }
 
 getFilter <- function(df,INPUTID,columna){
+  loggVar("getFilter",INPUTID)
   if (!(columna %in% colnames(df))) {
     columna <- colnames(df)[1]
   }
@@ -123,9 +124,27 @@ myCheckboxGroupInput <- function(INPUTID,delectenda,NOMEN="select",iL=TRUE,anzSe
 
 serverFunction <- function(input, output) {
   
-  columUnum <- reactive(x={return(getNonNullValue(wert=input$columUnum,ersatz=kpis[1]))})
-  
-  columDuo <- reactive(x={return(getNonNullValue(wert=input$columDuo,ersatz=kpis[length(kpis)]))})
+  filterDf <- function(df) {
+    resultat <- df
+    filtres <- input$filters
+    logg("filterDf",getVectorLogMsg(filtres))
+    for (f in filtres) {
+      filterRangeVar <- paste(f,"Filter",sep="")
+      loggVar("filterDf",filterRangeVar)
+      filterRange <- input[[filterRangeVar]]
+      logg("filterDf",getVectorLogMsg(filterRange))
+      filterCol <- as.vector(resultat[,c(f)])
+      if (0 < length(filterRange)) {
+        if (class(filterCol) %in% c(kpiTypes,timeTypes)) {
+          resultat <- resultat[(filterRange[1]<=filterCol) & (filterCol<=filterRange[2]),]
+        } else {
+          resultat <- resultat[filterCol %in% filterRange,]
+        }
+      }
+    }
+    loggResultatDf(FUN="filterDf",df=resultat)
+    return(resultat)
+  }
   
   abszissenCols  <- reactive(x={
     if ("none" == input$abszissenOP) {
@@ -146,11 +165,11 @@ serverFunction <- function(input, output) {
     }
     return(result)
   })
-
+  
   analyseWuerfel <- reactive(x={
     input$setDimButton
     isolate(expr={
-      resultat <- getData(dimensionen=input$dim,desiredCols=c(columUnum(),columDuo()))
+      resultat <- getData(dimensionen=input$dim,desiredCols=kpis)
     })
     loggResultatDf("analyseWuerfel",df=resultat)
     return(resultat)
@@ -194,25 +213,27 @@ serverFunction <- function(input, output) {
     })
   })
   
-  output$columUnumSelector <- renderUI({
+  output$FilterSelector <- renderUI({
     input$setDimButton
     isolate(expr={
       delectendum <- c(input$dim,kpis)
-      getSelector(INPUTID="columUnum",NOMEN="Filter 1/ filtre 1",delectendum=delectendum)
+      getSelector(INPUTID="filters",NOMEN="Filter / filtres",delectendum=delectendum,multi=TRUE)
     })
   })
   
-  output$columDuoSelector <- renderUI({
+  output$Filter <- renderUI({
+    input$getFilterButton
     input$setDimButton
     isolate(expr={
-      delectendum <- c(input$dim,kpis)
-      getSelector(INPUTID="columDuo",NOMEN="Filter 2/ filtre 2",delectendum=delectendum)
+      filtres <- input$filters
+      resultat <- list()
+      logg("output$Filter",getVectorLogMsg(filtres))
+      for (f in filtres){
+        resultat <- list(resultat,getFilter(df=analyseWuerfel(),INPUTID=paste(f,"Filter",sep=""),columna=f))
+      }
+      return(resultat)
     })
   })
-  
-  output$columUnumFilter <- renderUI({getFilter(df=analyseWuerfel(),INPUTID="anacolumUnum",columna=columUnum())})
-  
-  output$columDuoFilter <- renderUI({getFilter(df=analyseWuerfel(),INPUTID="anacolumDuo",columna=columDuo())})
   
   analyseDaten <- reactive(x={
     input$goButton
@@ -220,42 +241,11 @@ serverFunction <- function(input, output) {
     isolate(expr={
       abszissenData <- abszissenCols()
       ordinatenData <- ordinatenCols()
-      colUnumData <- c(columUnum())
-      colDuoData <- c(columDuo())
       gruppierung <- getNonNullValue(wert=input$Gruppierung,ersatz=grps[1])
-      anacolumUnum <- getNonNullValue(wert=input$anacolumUnum,ersatz=c(0,1))
-      anacolumDuo <- getNonNullValue(wert=input$anacolumDuo,ersatz=c(0,1))
-      neededKpis <- intersect(x=kpis,y=c(abszissenData,ordinatenData,gruppierung,colUnumData,colDuoData))
+      neededKpis <- intersect(x=kpis,y=c(abszissenData,ordinatenData,gruppierung,input$filters))
       logg("analyseDaten",getVectorLogMsg(neededKpis))
-      granular <- getData(dimensionen=input$dim,desiredCols=neededKpis)
-
-      # ColumnUnum
-      granular$cUnum <- as.vector(granular[,c(colUnumData)])
-      granular <- granular[!is.na(granular$cUnum),]
-      # ColumnDuo
-      granular$cDuo <- as.vector(granular[,c(colDuoData)])
-      granular <- granular[!is.na(granular$cDuo),]
+      granular <- getData(dimensionen=input$dim,desiredCols=neededKpis) %>% filterDf
       
-      colsToSelect <- as.vector(unique(c(abszissenData,ordinatenData,"cDuo",gruppierung)))
-
-      # Filter 1 anwenden
-      granColUnum <- as.vector(granular$cUnum)
-
-      if (class(granColUnum) %in% c(kpiTypes,timeTypes)) {
-        granular <- granular[(anacolumUnum[1]<=granColUnum) & (granColUnum<=anacolumUnum[2]),colsToSelect]
-      } else {
-        granular <- granular[granColUnum %in% anacolumUnum,colsToSelect]
-      }
-
-      # Filter 2 anwenden
-      insColDuo <- as.vector(granular$cDuo)
-      
-      if (class(insColDuo) %in% c(kpiTypes,timeTypes)) {
-        granular <- granular[(input$anacolumDuo[1]<=insColDuo) & (insColDuo<=input$anacolumDuo[2]),colsToSelect]
-      } else {
-        granular <- granular[insColDuo %in% input$anacolumDuo,colsToSelect]
-      }
-
       if ("none" == input$abszissenOP) {
         granular$x <- granular[,c(abszissenData)]
       } else {
@@ -268,14 +258,14 @@ serverFunction <- function(input, output) {
         granular <- opColumn(dataFrame=granular,operator=input$ordinatenOP,fstCol=ordinatenData[1],sndCol=ordinatenData[2],newCol="y")
       }
       granular <- granular[!is.na(granular$y),]
-
+      
       if (class(granular$y) %in% kpiTypes) {
         byFrame <- granular[,c("x",gruppierung)]
         resultat <- aggDf(x=list(y=granular$y),by=byFrame,FUN=input$aggFun)
       } else {
         resultat <- unique(granular)
       }
-
+      
       # resultat$Gruppe <- as.vector(as.character(resultat[,c(gruppierung)]))
       resultat$Gruppe <- resultat[,c(gruppierung)]
       resultat <- sortFrameByFstCol(dataFrame=resultat[!is.na(resultat$y),c("Gruppe","x","y")])
@@ -287,29 +277,29 @@ serverFunction <- function(input, output) {
   observe(x={
     input$goButton
     # isolate(expr={
-      xTitel <- getFormula(operator=input$abszissenOP,xVec=abszissenCols())
-      yTitel <- getFormula(operator=input$ordinatenOP,xVec=ordinatenCols())
-      loggVar("observe",yTitel)
-      gruppierung <- getNonNullValue(wert=input$Gruppierung,ersatz=grps[1])
-      titel <- ""
-      analyseShowValueFun <- function(df){
-        if ("y" %in% colnames(x=df)) {
-          xClass <- class(inputDf[[abszissenCols()[1]]])
-          if (xClass %in% timeTypes) {df$xZeig <- milliSecondsToString(t=df$x,Zeitformat=getTimeFormat(xClass))} else {df$xZeig <- df$x}
-          yClass <- class(inputDf[[ordinatenCols()[1]]])
-          if (yClass %in% timeTypes) {df$yZeig <- milliSecondsToString(t=df$y,Zeitformat=getTimeFormat(yClass))} else {df$yZeig <- df$y}
-          zClass <- class(inputDf[[c(gruppierung)]])
-          if (zClass %in% timeTypes) {df$zZeig <- milliSecondsToString(t=df$Gruppe,Zeitformat=getTimeFormat(zClass))} else {df$zZeig <- df$Gruppe}
-          resultat <-zeigeWert(dataFramito=df)
-        } else {resultat <- NA}
-        return(resultat)
-      }
-      
-      plot <- getGgvisPlot(df=analyseDaten(),trend=as.logical(input$Analysentrend)
-                        ,xTitel=xTitel,yTitel=yTitel,plotTitel=titel,gesamtgruppenname=NA
-                        ,sizeFactor=c(6/5,8/9),showValueFun=analyseShowValueFun)
-      logg("observe","bind_shiny(plot,analysePlot)")
-      bind_shiny(plot,"analysePlot")
+    xTitel <- getFormula(operator=input$abszissenOP,xVec=abszissenCols())
+    yTitel <- getFormula(operator=input$ordinatenOP,xVec=ordinatenCols())
+    loggVar("observe",yTitel)
+    gruppierung <- getNonNullValue(wert=input$Gruppierung,ersatz=grps[1])
+    titel <- ""
+    analyseShowValueFun <- function(df){
+      if ("y" %in% colnames(x=df)) {
+        xClass <- class(inputDf[[abszissenCols()[1]]])
+        if (xClass %in% timeTypes) {df$xZeig <- milliSecondsToString(t=df$x,Zeitformat=getTimeFormat(xClass))} else {df$xZeig <- df$x}
+        yClass <- class(inputDf[[ordinatenCols()[1]]])
+        if (yClass %in% timeTypes) {df$yZeig <- milliSecondsToString(t=df$y,Zeitformat=getTimeFormat(yClass))} else {df$yZeig <- df$y}
+        zClass <- class(inputDf[[c(gruppierung)]])
+        if (zClass %in% timeTypes) {df$zZeig <- milliSecondsToString(t=df$Gruppe,Zeitformat=getTimeFormat(zClass))} else {df$zZeig <- df$Gruppe}
+        resultat <-zeigeWert(dataFramito=df)
+      } else {resultat <- NA}
+      return(resultat)
+    }
+    
+    plot <- getGgvisPlot(df=analyseDaten(),trend=as.logical(input$Analysentrend)
+                         ,xTitel=xTitel,yTitel=yTitel,plotTitel=titel,gesamtgruppenname=NA
+                         ,sizeFactor=c(1,8/9),showValueFun=analyseShowValueFun)
+    logg("observe","bind_shiny(plot,analysePlot)")
+    bind_shiny(plot,"analysePlot")
     # })
   })
 }
@@ -329,37 +319,36 @@ uiFunction <- function() {
                                                         ,selectize=FALSE
                                                         ,multiple=FALSE
                                                         ,choices=c("none","+","-","*","/")))
-                             ,column(width=5,uiOutput("abszissenSelector"))
-                             ,column(width=1,selectInput(inputId="ordinatenOP"
-                                                         ,label="operator"
-                                                         ,selected="none"
-                                                         ,selectize=FALSE
-                                                         ,multiple=FALSE
-                                                         ,choices=c("none","+","-","*","/")))
-                             ,column(width=5,uiOutput("ordinatenSelector"))
-                   )
-                   ,fluidRow(column(width=2,uiOutput("gruppenSelector"))
-                             ,column(width=2,uiOutput("columUnumSelector"))
-                             ,column(width=2,uiOutput("columDuoSelector"))
-                             ,column(width=2,selectInput(inputId="aggFun"
-                                                         ,label="aggregieren / agréger"
-                                                         ,selected="sum"
-                                                         ,selectize=FALSE
-                                                         ,multiple=FALSE
-                                                         ,choices=c("sum","mean","median","min","max"
-                                                                    ,"sd","var","IQR"))
-                             )
-                   )
-                   ,fluidRow(column(width=5,uiOutput("columUnumFilter"))
-                             ,column(width=5,uiOutput("columDuoFilter"))
+                             ,column(width=7,uiOutput("abszissenSelector"))
+                             ,column(width=2,uiOutput("gruppenSelector"))
                              ,column(width=1,selectInput(inputId="Analysentrend"
                                                          ,label="Trend"
                                                          ,multiple=FALSE
                                                          ,choices=as.logical(c(FALSE,TRUE))
                                                          ,selectize=TRUE
                                                          ,selected=FALSE))
+                   )
+                   ,fluidRow(column(width=1,selectInput(inputId="ordinatenOP"
+                                                        ,label="operator"
+                                                        ,selected="none"
+                                                        ,selectize=FALSE
+                                                        ,multiple=FALSE
+                                                        ,choices=c("none","+","-","*","/")))
+                             ,column(width=7,uiOutput("ordinatenSelector"))
+                             ,column(width=2,selectInput(inputId="aggFun"
+                                                         ,label="aggregieren / agréger"
+                                                         ,selected="sum"
+                                                         ,selectize=FALSE
+                                                         ,multiple=FALSE
+                                                         ,choices=c("sum","mean","median","min","max"
+                                                                    ,"sd","var","IQR")))
                              ,column(width=1,actionButton(inputId="goButton",label="Go!"))
                    )
-                   ,fluidRow(column(width=12,ggvisOutput(plot_id="analysePlot")))
-                   
+                   ,sidebarLayout(
+                     mainPanel(ggvisOutput(plot_id="analysePlot"))
+                     ,sidebarPanel(
+                       fluidRow(column(width=10,uiOutput("FilterSelector"))
+                                ,column(width=2,actionButton(inputId="getFilterButton",label="get Filter")))
+                       ,htmlOutput("Filter")
+                     ))
   ))}
